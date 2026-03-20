f0ee0629a55f7c6f700946687accb0495f07e36f
Sunil Kumar Nahak <nahaksunilkumara@gmail.com>
2026-03-20 19:38:22 +0530

    Implement Number Guessing Game
    This program allows users to guess a randomly generated number between 1 and 100, providing feedback on each guess.

---
+++ NumberGuessingGame.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int number = rand.nextInt(100) + 1; // 1 to 100
+        int guess;
+        int attempts = 0;
+
+        System.out.println("===== Number Guessing Game =====");
+        System.out.println("Guess a number between 1 and 100");
+
+        do {
+            System.out.print("Enter your guess: ");
+            guess = sc.nextInt();
+            attempts++;
+
+            if (guess > number) {
+                System.out.println("Too High!");
+            } else if (guess < number) {
+                System.out.println("Too Low!");
+            } else {
+                System.out.println("Correct! 🎉");
+            }
+
+        } while (guess != number);
+
+        System.out.println("You guessed in " + attempts + " attempts.");
+    }
+}
